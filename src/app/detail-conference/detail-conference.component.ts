2559cd02a445b85b7fcb744f2726542d13ff32a0
Roberto Castro <robertocastroroy@gmail.com>
2017-02-07 17:10:17 +0100

    Test

--- src/app/detail-conference/detail-conference.component.ts
+++ src/app/detail-conference/detail-conference.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute} from "@angular/router";
 import {UpdateConferenceService} from "../services/update.conferences.service";
 import { Location }  from '@angular/common';
 import {Subscription} from "rxjs";
+import { StoreConferenceService } from '../services/store.conferences.service';
 
 @Component({
   selector: 'app-detail-conference',
@@ -13,29 +14,57 @@ import {Subscription} from "rxjs";
 export class DetailConferenceComponent implements OnInit, OnDestroy {
 
   /*mode input*/
-  @Input() conferenceSelected:Conference;
+  @Input() conferenceSelected: Conference;
 
   conferenceIdSubscription: Subscription;
+  conferencesSubscription: Subscription;
+  //conferences: Conference[];
 
-  constructor(public params: ActivatedRoute, public updateConference: UpdateConferenceService, private location: Location) { }
+  constructor(
+    public params: ActivatedRoute,
+    public updateConference: UpdateConferenceService,
+    private location: Location,
+    private store: StoreConferenceService
+  ) { }
 
 
   ngOnInit() {
+
+
+
+
     /*mode naivagte*/
     let confId:any;
-    this.conferenceIdSubscription = this.params.queryParams.subscribe(observable=> confId = observable);
+    let conferences: Conference[];
+    this.conferenceIdSubscription = this.params.queryParams.subscribe(observable => confId = observable);
 
+
+    this.conferencesSubscription = this.store.conference$.subscribe(
+      observable => {
+        conferences = observable;
+
+        console.log(conferences);
+        this.conferenceSelected = conferences.find(conference => conference.id == confId.confId);
+        console.log(this.conferenceSelected);
+      }
+    );
+
+
+
+    /*
     this.updateConference.getConferenceById(Number(confId.confId)).then(
     //this.updateConference.getConferenceById(2).then(
       (conferences:any) => {
         console.log(conferences);
         this.conferenceSelected = conferences;}
     )
+    */
   }
 
   ngOnDestroy(){
     console.log('destroy conferenceidsub');
     this.conferenceIdSubscription.unsubscribe();
+    this.conferencesSubscription.unsubscribe();
   }
 
   goBack(): void {
